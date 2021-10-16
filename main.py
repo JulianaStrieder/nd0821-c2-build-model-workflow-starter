31efa019a7bcab975870e2af89e6d4857d824b40
Juliana Strieder Philippsen <striederjp@gmail.com>
2021-10-16 13:48:16 -0300

    add channel to conda.yml files to resolve conflicts and change way to read config with hydra

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from omegaconf import DictConfig
 _steps = [
     "download",
     "basic_cleaning",
-    "data_check",
-    "data_split",
-    "train_random_forest",
+    # "data_check",
+    # "data_split",
+    # "train_random_forest",
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
@@ -21,7 +21,7 @@ _steps = [
 
 
 # This automatically reads in the configuration
-@hydra.main(config_name='config')
+# @hydra.main(config_name='config')
 def go(config: DictConfig):
 
     # Setup the wandb experiment. All runs will be grouped under this name
@@ -92,4 +92,7 @@ def go(config: DictConfig):
 
 
 if __name__ == "__main__":
-    go()
+    c = []
+    hydra.main(config_path="config.yaml")(c.append)()
+    config = c[0]
+    go(config)
